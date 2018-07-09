01a2d08dc430978342b458ab8788745f29e126c2
AdityaCZ <Aditya.Kumar@cognizant.com>
2018-07-09 16:12:40 +0530

    Demo with shila

--- com.roi.selenium/src/test/java/com/roi/selenium/Demo1.java
+++ com.roi.selenium/src/test/java/com/roi/selenium/Demo1.java
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-//here
+//here comment
 public class Demo1 {
 
 	WebDriver Driver;
